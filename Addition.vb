9bd720297a2df524d9f79dff5944127ab1c404a5
Kartikey Tripathi <31284213+kartikeytripathi@users.noreply.github.com>
2017-09-03 20:21:07 +0530

    Addition of Two Numbers
    Program to Display Addition of two numbers.

---
+++ Addition.vb
@@ -0,0 +1,14 @@
+﻿Public Class Addition
+
+    Dim num1 As Integer
+    Dim num2 As Integer
+    Dim addition As Integer
+
+    Private Sub Add_Click(sender As System.Object, e As System.EventArgs) Handles Add.Click
+
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        addition = num1 + num2
+        MessageBox.Show(addition, "Addition")
+    End Sub
+End Class
