7dddd4bc1dabf4563931f6c17f7fb1f1f765ce55
Kartikey Tripathi <31284213+kartikeytripathi@users.noreply.github.com>
2017-09-03 16:28:07 +0530

    Area of Circle
    Source Code +  Word Document of Project

---
+++ AREA OF CIRCLE.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Dim rad As Double
+    Dim ar As Double
+
+    Private Sub PictureBox1_Click(sender As System.Object, e As System.EventArgs)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles TextBox.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Area.Click
+
+        rad = TextBox.Text
+        ar = 3.14 * 3.14 * rad
+        MessageBox.Show(ar, "Area of Circle")
+
+
+    End Sub
+End Class
